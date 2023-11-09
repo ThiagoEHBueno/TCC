d6e5100713941a4706f3c2dfdb0ead8d15c2c71c
ThiagoEHBueno <79876418+ThiagoEHBueno@users.noreply.github.com>
2023-11-08 21:49:51 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TelaInicialComponent } from './components/tela-inicial/tela-inicial.component';
+import { TelaDeLoginComponent } from './components/tela-de-login/tela-de-login.component';
+import { CadastroProfessorComponent } from './components/cadastro-professor/cadastro-professor.component';
+import { CadastroAlunoComponent } from './components/cadastro-aluno/cadastro-aluno.component';
+import { TelaProfessorComponent } from './components/tela-professor/tela-professor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TelaInicialComponent,
+    TelaDeLoginComponent,
+    CadastroProfessorComponent,
+    CadastroAlunoComponent,
+    TelaProfessorComponent,
   ],
   imports: [
     BrowserModule,
