d76f720a2622698459acf4419d9e7936cb2230a9
ThiagoEHBueno <thiagoeduardohonoriobueno@hotmail.com>
2023-11-15 17:40:00 -0300

    Cadastro prof

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaInicialComponent } from './components/tela-inicial/tela-inicial.component';
@@ -8,6 +7,11 @@ import { TelaDeLoginComponent } from './components/tela-de-login/tela-de-login.c
 import { CadastroProfessorComponent } from './components/cadastro-professor/cadastro-professor.component';
 import { CadastroAlunoComponent } from './components/cadastro-aluno/cadastro-aluno.component';
 import { TelaProfessorComponent } from './components/tela-professor/tela-professor.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
@@ -17,12 +21,17 @@ import { TelaProfessorComponent } from './components/tela-professor/tela-profess
     CadastroProfessorComponent,
     CadastroAlunoComponent,
     TelaProfessorComponent,
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
