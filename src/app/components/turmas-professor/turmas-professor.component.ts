a3552acd90be81eb09ca42f1c361e251c8f8ff45
AeronBorjil <joaommmotta@gmail.com>
2023-11-24 17:08:20 -0300

    tela de turmas

---
+++ src/app/components/turmas-professor/turmas-professor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-turmas-professor',
+  templateUrl: './turmas-professor.component.html',
+  styleUrls: ['./turmas-professor.component.css"]
+})
+export class TelaProfessorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
